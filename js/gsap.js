322c1df1a0a83e9d216984c5a4b3d1e7910d4d06
victoria haruka <vic.haruka07@gmail.com>
2025-10-22 23:21:51 -0300

    va

---
+++ js/gsap.js
@@ -0,0 +1,45 @@
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".containerPai",
+    start: "top top",
+    end: "+=2000",
+    onLeave: () => (window.location.href = "index.html"),
+    markers: true,
+    pin: true,
+    scrub: 2,
+  },
+});
+
+tl.to(".secao1", {
+  WebkitMaskSize: "20vw",
+  maskSize: "20vw",
+  duration: 2,
+});
+
+tl.to(
+  ".logoBg",
+  {
+    opacity: 0,
+    duration: 0.5,
+  },
+  "-=1.5"
+);
+
+tl.to(
+  ".secaoBranca",
+  {
+    backgroundColor: "white",
+    duration: 1,
+  },
+  "-=1"
+);
+
+tl.from(
+  ".secao2 img",
+  {
+    opacity: 0,
+    filter: "blur(20px)",
+    duration: 0.5,
+  },
+  "+=.3"
+);
