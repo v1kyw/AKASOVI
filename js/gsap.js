e9c154a7f75756fa1d0b2eb92916b6601198f410
victoria haruka <vic.haruka07@gmail.com>
2025-10-23 00:49:56 -0300

    vm

--- js/gsap.js
+++ js/gsap.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   gsap.registerPlugin(ScrollTrigger);
 
   const tl = gsap.timeline({
@@ -13,28 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  tl.to(".secao1", {
-    WebkitMaskSize: "20vw",
-    maskSize: "20vw",
-    duration: 2,
-  });
+  tl.to(".secao1", { WebkitMaskSize: "20vw", maskSize: "20vw", duration: 2 });
+  tl.to(".secaoBranca", { backgroundColor: "white", duration: 1 }, "-=1");
+  tl.from(".secao2 img", { opacity: 0, filter: "blur(20px)", duration: 0.5 }, "+=.3");
 
-  tl.to(
-    ".secaoBranca",
-    {
-      backgroundColor: "white",
-      duration: 1,
-    },
-    "-=1"
-  );
-
-  tl.from(
-    ".secao2 img",
-    {
-      opacity: 0,
-      filter: "blur(20px)",
-      duration: 0.5,
-    },
-    "+=.3"
-  );
+  ScrollTrigger.refresh();
 });
